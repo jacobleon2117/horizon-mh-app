38f9b7b12ee3988338aafe21241556e8797ab347
jacobleon2117 <jacobleon2117@gmail.com>
2024-12-18 04:36:16 -0800

    fixed missing file + .env

---
+++ client/src/api/index.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export const journalsApi = {
+  getAll: () => api.get("/journals"),
+  create: (data) => api.post("/journals", data),
+  update: (id, data) => api.put(`/journals/${id}`, data),
+  delete: (id) => api.delete(`/journals/${id}`),
+};
+
+export default api;
