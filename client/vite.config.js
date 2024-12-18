1e03ba3f16a5b7b0d1a44245d5c4e817064848fd
jacobleon2117 <jacobleon2117@gmail.com>
2024-12-18 03:34:03 -0800

    fixing

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,7 +11,12 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: [
+        "react",
+        "react-dom",
+        "react/jsx-runtime",
+        "react/jsx-dev-runtime",
+      ],
     },
   },
   server: {
